b5be4a6828accfc7c0e51c926c69bc33c6b74fc1
0vs3pyan <105593925+0vs3pyan@users.noreply.github.com>
2022-05-15 17:50:52 +0300

    Add files via upload

---
+++ shop/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Product, Category, Basket, Order
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Basket)
+admin.site.register(Order)
---
+++ shop/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from .models import Product
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = {
+            'title',
+            'text',
+            'price',
+            'cat',
+        }
+
+# class OrderForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = {
+#             'first_name',
+#             'second_name',
+#             'email',
+#         }
